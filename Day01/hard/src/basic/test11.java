73ce73b5c6140bde1bcfad91ddc8b07bfcecc643
changsoon123 <sool0458@naver.com>
2023-02-25 11:41:20 +0900

    hey

--- Day01/hard/src/basic/test11.java
+++ Day01/hard/src/basic/test11.java
@@ -3,8 +3,28 @@ package basic;
 public class test11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	int[] numArr = new int[10];
+	int[] count = new int[10];
+	
+	for (int i=0; i< numArr.length;i++) {
+		numArr[i] = (int)(Math.random()*10);
+		System.out.print(numArr[i]);
+	}
+	System.out.println();
+	
+	for(int i=0;i<numArr.length;i++) {
+		count[numArr[i]]++;
+		
+	}
+	
+	for(int i=0;i<numArr.length;i++) {
+		System.out.println(i+"의 개수: "+count[i]);
+		
+	}
 
+
+	
 	}
 
 }
